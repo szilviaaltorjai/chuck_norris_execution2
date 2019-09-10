a8bb47d9f553a35a38e94014e4fc25226988c06d
Austin Lasseter <austinlasseter@gmail.com>
2019-09-10 17:36:32 -0400

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# chuck_norris_execution
-Pick your preferred method of execution by the famous actor who brought you Walker Texas Ranger.
+## Example dashboard using [Plotly Dash](https://plot.ly/dash).
 
-https://chuck-norris-fist.herokuapp.com/
+* You can view the finished app on heroku [here](https://chuck-norris-execution.herokuapp.com/).
+* Read more about interactive callbacks [here](https://dash.plot.ly/getting-started-part-2)
+* My full tutorial on plotly dash is [available on github](https://github.com/austinlasseter/plotly_dash_tutorial
+* Note: the `assets` folder contains a file called `favicon.ico` -- you can find and download customized favicons [here](https://www.favicon.cc/). Just replace the current favicon with a new one.
--- app.py
+++ app.py
@@ -6,23 +6,28 @@ import os
 
 ###### Set up variables
 list_of_choices=['punch', 'body-slam', 'round-house kick to the face']
-myheading='Chuck Norris execution method'
-tabtitle='Chuck'
+githublink = 'https://github.com/austinlasseter/chuck_norris_execution'
+image1='chucknorris.jpg'
+
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title=tabtitle
+app.title='Chuck'
 
 ####### Layout of the app ########
 app.layout = html.Div([
-    html.H2(myheading),
-    dcc.Dropdown(
-        id='your-input-here',
-        options=[{'label': i, 'value': i} for i in list_of_choices],
-        value='punch'
-    ),
-    html.Div(id='your-output-here', children='')
+    html.H2('Chuck Norris execution method'),
+    html.Img(src=app.get_asset_url(image1), style={'width': 'auto', 'height': '10%'}),
+    dcc.Dropdown(id='your-input-here',
+                options=[{'label': i, 'value': i} for i in list_of_choices],
+                value='punch',
+                style={'width': '500px'}),
+    html.Br(),
+    html.Div(id='your-output-here', children=''),
+    html.Br(),
+    html.A('Code on Github', href=githublink),
+
 ])
 
 
@@ -30,7 +35,7 @@ app.layout = html.Div([
 @app.callback(dash.dependencies.Output('your-output-here', 'children'),
               [dash.dependencies.Input('your-input-here', 'value')])
 def display_value(whatever_you_chose):
-    return f'Chuck Norris will execute by {whatever_you_chose}'
+    return f'Chuck Norris will now execute you with a {whatever_you_chose}.'
 
 
 ######### Run the app #########
